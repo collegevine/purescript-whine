ce8da88a828282af3ef2b6761348c6da57fe524e
Fyodor Soikin <name.fa@gmail.com>
2024-12-30 00:11:21 -0500

    Remove tracing

--- src/Whine/Runner/Glob.js
+++ src/Whine/Runner/Glob.js
@@ -1,8 +1,4 @@
 import mm from 'micromatch'
 export { globSync } from 'glob'
 
-export const test = patterns => file => {
-  const res = mm.isMatch(file, patterns)
-  console.log('test', file, patterns, res)
-  return res
-}
+export const test = patterns => file => mm.isMatch(file, patterns)
